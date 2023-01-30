4a309b8a85f542762f309878f5fbc61b75d2e7d6
RubenTeles <ruben-teles@hotmail.com>
2023-01-30 17:04:46 +0000

    ex03 done!

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 16:00:04 by rteles            #+#    #+#             */
-/*   Updated: 2023/01/30 16:54:19 by rteles           ###   ########.fr       */
+/*   Updated: 2023/01/30 17:01:20 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 //Method Barycentric
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-    float lambda1 = ((b.getY().toFloat() - c.getY().toFloat()) * (point.getX().toFloat() - c.getX().toFloat()) + (c.getX().toFloat() - b.getX().toFloat()) * (point.getY().toFloat() - c.getY().toFloat())) / ((b.getY().toFloat() - c.getY().toFloat()) * (a.getX().toFloat() - c.getX().toFloat()) + (c.getX().toFloat() - b.getX().toFloat()) * (a.getY().toFloat() - c.getY().toFloat()));
-    float lambda2 = ((c.getY().toFloat() - a.getY().toFloat()) * (point.getX().toFloat() - c.getX().toFloat()) + (a.getX().toFloat() - c.getX().toFloat()) * (point.getY().toFloat() - c.getY().toFloat())) / ((b.getY().toFloat() - c.getY().toFloat()) * (a.getX().toFloat() - c.getX().toFloat()) + (c.getX().toFloat() - b.getX().toFloat()) * (a.getY().toFloat() - c.getY().toFloat()));
-    float lambda3 = 1 - lambda1 - lambda2;
-    
-    if (lambda1 > 0 && lambda1 < 1 && lambda2 > 0 && lambda2 < 1 && lambda3 > 0 && lambda3 < 1)
-        return true;
-    return false;
+	float lambda1 = ((b.getY().toFloat() - c.getY().toFloat()) * (point.getX().toFloat() - c.getX().toFloat()) + (c.getX().toFloat() - b.getX().toFloat()) * (point.getY().toFloat() - c.getY().toFloat())) / ((b.getY().toFloat() - c.getY().toFloat()) * (a.getX().toFloat() - c.getX().toFloat()) + (c.getX().toFloat() - b.getX().toFloat()) * (a.getY().toFloat() - c.getY().toFloat()));
+	float lambda2 = ((c.getY().toFloat() - a.getY().toFloat()) * (point.getX().toFloat() - c.getX().toFloat()) + (a.getX().toFloat() - c.getX().toFloat()) * (point.getY().toFloat() - c.getY().toFloat())) / ((b.getY().toFloat() - c.getY().toFloat()) * (a.getX().toFloat() - c.getX().toFloat()) + (c.getX().toFloat() - b.getX().toFloat()) * (a.getY().toFloat() - c.getY().toFloat()));
+	float lambda3 = 1 - lambda1 - lambda2;
+	
+	if (lambda1 > 0 && lambda1 < 1 && lambda2 > 0 && lambda2 < 1 && lambda3 > 0 && lambda3 < 1)
+		return true;
+	return false;
 }
+
+//If any lambda is <= 0 or >= 1 the pointer is out of the triangle or in vertex or on edge.
